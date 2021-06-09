771870ab1b6caf2c191bf562c07f74f8077309b3
Alexander Vitchinov <vitalex92@yandex.ru>
2021-06-09 23:18:31 +0300

    bugfix v2

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,16 +1,9 @@
-import { render } from '@testing-library/react';
 import StartScreen from '../StartScreen/StartScreen';
 import Header from '../Header/Header';
 import Playfield from '../Playfield/Playfield';
 import Standings from '../Standings/Standings';
 
 function App({ store }) {
-  store.subscribe(render);
-  const unsubscribe = store.subscribe(() =>
-    console.log('State after dispatch: ', store.getState())
-  );
-
-  unsubscribe();
   return (
     <div className='App'>
       <StartScreen />
